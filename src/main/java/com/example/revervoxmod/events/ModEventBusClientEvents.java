0a6a674314a0ca4814a1214949c0cef3a3bdb5ba
AnimalRacional <88465145+AnimalRacional@users.noreply.github.com>
2025-08-03 14:01:44 +0100

    Fix dedicated server crash: RegisterParticleProvidersEvent should only be used on the client

--- src/main/java/com/example/revervoxmod/events/ModEventBusClientEvents.java
+++ src/main/java/com/example/revervoxmod/events/ModEventBusClientEvents.java
@@ -5,10 +5,13 @@ import com.example.revervoxmod.entity.client.ModModelLayers;
 import com.example.revervoxmod.entity.client.RevervoxGeoRenderer;
 import com.example.revervoxmod.entity.client.ThingyModel;
 import com.example.revervoxmod.entity.client.ThingyRenderer;
+import com.example.revervoxmod.particle.custom.RevervoxParticles;
 import com.example.revervoxmod.registries.EntityRegistry;
+import com.example.revervoxmod.registries.ParticleRegistry;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -24,5 +27,11 @@ public class ModEventBusClientEvents {
     public static void onClientSetup(FMLClientSetupEvent event) {
         EntityRenderers.register(EntityRegistry.THINGY.get(), ThingyRenderer::new);
         EntityRenderers.register(EntityRegistry.REVERVOX_GEO.get(), RevervoxGeoRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void registerParticleFactories(final RegisterParticleProvidersEvent event) { //Pode n ser este evento
+        event.registerSpriteSet(ParticleRegistry.REVERVOX_PARTICLES.get(),
+                RevervoxParticles.Provider::new);
     }
 }
--- src/main/java/com/example/revervoxmod/events/ModEventBusEvents.java
+++ src/main/java/com/example/revervoxmod/events/ModEventBusEvents.java
@@ -3,10 +3,7 @@ package com.example.revervoxmod.events;
 import com.example.revervoxmod.RevervoxMod;
 import com.example.revervoxmod.entity.custom.RevervoxGeoEntity;
 import com.example.revervoxmod.entity.custom.ThingyEntity;
-import com.example.revervoxmod.particle.custom.RevervoxParticles;
 import com.example.revervoxmod.registries.EntityRegistry;
-import com.example.revervoxmod.registries.ParticleRegistry;
-import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,12 +15,4 @@ public class ModEventBusEvents {
         event.put(EntityRegistry.THINGY.get(), ThingyEntity.createAttributes().build());
         event.put(EntityRegistry.REVERVOX_GEO.get(), RevervoxGeoEntity.createAttributes().build());
     }
-
-
-    @SubscribeEvent
-    public static void registerParticleFactories(final RegisterParticleProvidersEvent event) { //Pode n ser este evento
-        event.registerSpriteSet(ParticleRegistry.REVERVOX_PARTICLES.get(),
-                RevervoxParticles.Provider::new);
-    }
-
 }
