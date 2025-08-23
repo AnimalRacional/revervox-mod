8f812e8236450ee8f31a840ded543fa1a77a40e4
Omiao <88465145+AnimalRacional@users.noreply.github.com>
2025-08-23 21:04:59 +0100

    Fake bat event is now invisible to everyone other than the targeted player

--- src/main/java/dev/omialien/revervoxmod/config/RevervoxModServerConfigs.java
+++ src/main/java/dev/omialien/revervoxmod/config/RevervoxModServerConfigs.java
@@ -5,7 +5,6 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class RevervoxModServerConfigs {
     public static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     public static final ModConfigSpec.IntValue REVERVOX_MAX_AUDIOS = BUILDER.defineInRange("revervoxMaxAudios", 12, 1, 100);
-    // TODO grace periods não aparecem na config screen, talvez por serem floats
     public static final ModConfigSpec.DoubleValue REVERVOX_AFTER_SPEAK_GRACE_PERIOD = BUILDER.defineInRange("revervoxGracePeriod", 1.5d, 0d, 5d);
     public static final ModConfigSpec.DoubleValue REVERVOX_BAT_AFTER_SPAWN_GRACE_PERIOD = BUILDER.defineInRange("revervoxBatGracePeriod", 0.5d, 0d, 5d);
     public static final ModConfigSpec.IntValue REVERVOX_BAT_SPAWN_CHANCE = BUILDER.defineInRange("revervoxBatSpawnChance", 5, 2, 500);
