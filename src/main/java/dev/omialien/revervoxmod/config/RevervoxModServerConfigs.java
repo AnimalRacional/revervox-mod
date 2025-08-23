a3f5caddf74cc62257a01cf32622dc15d5996f40
Omiao <88465145+AnimalRacional@users.noreply.github.com>
2025-08-23 22:16:15 +0100

    Set block breaking to true as default

--- src/main/java/dev/omialien/revervoxmod/config/RevervoxModServerConfigs.java
+++ src/main/java/dev/omialien/revervoxmod/config/RevervoxModServerConfigs.java
@@ -28,7 +28,7 @@ public class RevervoxModServerConfigs {
         REVERVOX_SWORD_BONUS_DAMAGE = BUILDER.comment("How much extra damage the revervox sword deals to revervox entities").define("Revervox Sword Bonus Damage", 7);
         REVERVOX_BAT_TOOTH_DROP_CHANCE = BUILDER.comment("The chance of a revervox bat dropping its tooth on attack (1 in x)").define("Revervox Bat Tooth Drop Chance", 10);
         FAKE_BAT_EVENT_CHANCE = BUILDER.comment("Chance of fake bat event occuring. Higher is less likely, lower is more likely").define("Fake Bat Event Chance", 1.0d);
-        REVERVOX_BREAKS_BLOCKS = BUILDER.comment("Whether Revervox breaks blocks to get to you").define("Revervox breaks blocks", false);
+        REVERVOX_BREAKS_BLOCKS = BUILDER.comment("Whether Revervox breaks blocks to get to you").define("Revervox breaks blocks", true);
         REVERVOX_BREAKS_NONSOLID = BUILDER.comment("Whether Revervox breaks non-solid blocks such as torches and natural blocks such as dirt and stone").define("Revervox breaks non-solid blocks", true);
         REVERVOX_ABOVE_GROUND = BUILDER.comment("Whether Revervox spawns above ground").define("Revervox above ground", false);
         BUILDER.pop();
