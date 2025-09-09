c9615ecdfe64a9c735a5d29f331b3fb65f2ebae8
NalienDev <211351701+NalienDev@users.noreply.github.com>
2025-09-09 18:00:53 +0100

    Fixed RevervoxHurtByTargetGoal sound not playing & simplified it; Changed blockBreaking config name

--- src/main/java/dev/omialien/revervoxmod/entity/custom/RevervoxGeoEntity.java
+++ src/main/java/dev/omialien/revervoxmod/entity/custom/RevervoxGeoEntity.java
@@ -147,7 +147,7 @@ public class RevervoxGeoEntity extends Monster implements IRevervoxEntity, GeoEn
         this.goalSelector.addGoal(2, new EatFoodGoal(this, new ItemEntity(this.level(), this.getX(), this.getY(), this.getZ(), Items.FERMENTED_SPIDER_EYE.getDefaultInstance())));
         this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 0.7D, false));
         this.targetSelector.addGoal(1, new TargetSpokeGoal<>(this, this::isAngryAt, SoundRegistry.REVERVOX_ALERT.get(), SoundRegistry.REVERVOX_LOOP.get(), 50));
-        this.targetSelector.addGoal(2, new RevervoxHurtByTargetGoal(this, LivingEntity.class));
+        this.targetSelector.addGoal(2, new RevervoxHurtByTargetGoal(this));
         this.targetSelector.addGoal(3, new ResetUniversalAngerTargetGoal<>(this, false));
         this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Spider.class, true, true));
         this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, CaveSpider.class, true, true));
--- src/main/java/dev/omialien/revervoxmod/entity/goals/RevervoxHurtByTargetGoal.java
+++ src/main/java/dev/omialien/revervoxmod/entity/goals/RevervoxHurtByTargetGoal.java
@@ -4,60 +4,31 @@ import dev.omialien.revervoxmod.entity.custom.RevervoxGeoEntity;
 import dev.omialien.revervoxmod.networking.packets.SoundInstancePacket;
 import dev.omialien.revervoxmod.registries.SoundRegistry;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.GameRules;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 public class RevervoxHurtByTargetGoal extends HurtByTargetGoal {
-    private static final TargetingConditions HURT_BY_TARGETING = TargetingConditions.forCombat().ignoreLineOfSight().ignoreInvisibilityTesting();
     private final RevervoxGeoEntity revervox;
-    private int timestamp;
-    private final Class<?>[] toAllowDamage;
-    public RevervoxHurtByTargetGoal(RevervoxGeoEntity revervox, Class<?>... toAllowDamage) {
+    public RevervoxHurtByTargetGoal(RevervoxGeoEntity revervox) {
         super(revervox);
-        this.toAllowDamage = toAllowDamage;
         this.revervox = revervox;
-    }
-
-    public boolean canUse() {
-        int i = this.mob.getLastHurtByMobTimestamp();
-        LivingEntity livingentity = this.mob.getLastHurtByMob();
-        if (i != this.timestamp && livingentity != null) {
-            if (livingentity.getType() == EntityType.PLAYER && this.mob.level().getGameRules().getBoolean(GameRules.RULE_UNIVERSAL_ANGER)) {
-                return false;
-            } else {
-                boolean found = false;
-                for(Class<?> oclass : this.toAllowDamage) {
-                    if (oclass.isAssignableFrom(livingentity.getClass())) {
-                        found = true;
-                        break;
-                    }
-                }
-                if(found){
-                    return this.canAttack(livingentity, HURT_BY_TARGETING);
-                }else { return false; }
-            }
-        } else {
-            return false;
-        }
     }
 
     @Override
     public void start() {
-        if (!this.revervox.isAngry() && this.revervox.getTarget() instanceof Player) {
-            revervox.level().playSound(null, revervox.getX(), revervox.getY(), revervox.getZ(), SoundRegistry.REVERVOX_ALERT.get(), SoundSource.HOSTILE, 1.0F, 1.0F);
-            PacketDistributor.sendToPlayersTrackingEntity(this.mob, new SoundInstancePacket(
-                    this.mob.getId(),
-                    SoundRegistry.REVERVOX_LOOP.get(),
-                    SoundSource.HOSTILE,
-                    true
-            ));
-        }
-        timestamp = mob.getLastHurtByMobTimestamp();
         super.start();
+        if (this.revervox.getTarget() instanceof Player) {
+            revervox.level().playSound(null, revervox.getX(), revervox.getY(), revervox.getZ(), SoundRegistry.REVERVOX_ALERT.get(), SoundSource.HOSTILE, 1.0F, 1.0F);
+            PacketDistributor.sendToPlayersTrackingEntity(
+                    this.mob,
+                    new SoundInstancePacket(
+                            this.mob.getId(),
+                            SoundRegistry.REVERVOX_LOOP.get(),
+                            SoundSource.HOSTILE,
+                            true
+                    )
+            );
+        }
     }
 }
