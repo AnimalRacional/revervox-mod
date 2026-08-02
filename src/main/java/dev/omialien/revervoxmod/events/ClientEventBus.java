ecaf5146b942c233998cd01cbb08e6775c34b67f
omiao <omiaodev@gmail.com>
2026-08-02 21:49:40 +0100

    add tape random colors

--- src/main/java/dev/omialien/revervoxmod/events/ClientEventBus.java
+++ src/main/java/dev/omialien/revervoxmod/events/ClientEventBus.java
@@ -2,18 +2,25 @@ package dev.omialien.revervoxmod.events;
 
 import dev.omialien.revervoxmod.RevervoxMod;
 import dev.omialien.revervoxmod.entity.client.*;
+import dev.omialien.revervoxmod.items.TapeItem;
 import dev.omialien.revervoxmod.particle.custom.RevervoxParticles;
 import dev.omialien.revervoxmod.particle.custom.RevervoxSonicBoomParticle;
 import dev.omialien.revervoxmod.registries.EntityRegistry;
+import dev.omialien.revervoxmod.registries.ItemRegistry;
 import dev.omialien.revervoxmod.registries.ParticleRegistry;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import java.util.Objects;
+import java.util.UUID;
 
 @OnlyIn(Dist.CLIENT)
 @Mod.EventBusSubscriber(modid = RevervoxMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -35,5 +42,19 @@ public class ClientEventBus {
         event.registerSpriteSet(ParticleRegistry.REVERVOX_PARTICLES.get(),
                 RevervoxParticles.Provider::new);
         event.registerSpriteSet(ParticleRegistry.REVERVOX_SONIC_BOOM_PARTICLES.get(), RevervoxSonicBoomParticle.Provider::new);
+    }
+
+    @SubscribeEvent
+    public static void registerItemColors(final RegisterColorHandlersEvent.Item event) {
+        event.register((item, color) -> {
+            CompoundTag tag = item.getTag();
+            if (tag != null && tag.contains(TapeItem.PLAYER_ID)) {
+                UUID player = tag.getUUID(TapeItem.PLAYER_ID);
+                UUID audio = tag.getUUID(TapeItem.AUDIO_ID);
+                int hash = Objects.hash(player, audio);
+                return hash % 0xffffff;
+            }
+            return color == 1 ? 0 : -1;
+        }, ItemRegistry.TAPE.get());
     }
 }
